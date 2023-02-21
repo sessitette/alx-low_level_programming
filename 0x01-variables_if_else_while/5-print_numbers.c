5ccc96194d76d95183bd8e70430c975415f1e88e
sessitette <alexandertette@gmail.com>
2023-02-21 13:26:31 -0800

    Printing single digit decimals

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Function prints single digit base 10 numbers from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
