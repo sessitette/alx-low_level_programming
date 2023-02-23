c009160d9f77cd3ea58760a636cd8be84e91a30e
sessitette <alexandertette@gmail.com>
2023-02-23 13:01:42 -0800

    Using _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Uses _putchar function to print putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
