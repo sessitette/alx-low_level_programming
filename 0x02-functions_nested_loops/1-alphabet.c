8273be0ff02850b6708f95370d08ab3116e15ac9
sessitette <alexandertette@gmail.com>
2023-02-23 13:44:09 -0800

    Using print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Function prints the alphabets in lowercase
+ * Return: void (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
